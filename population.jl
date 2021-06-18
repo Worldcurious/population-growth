d6396f3f2ca9b060079ab1213b561ba815a3b265
Garrett Kinman <garrettkinman@gmail.com>
2021-06-18 11:48:38 -0400

    added script

---
+++ population.jl
@@ -0,0 +1,52 @@
+## imports
+
+using Plots
+using Noise
+
+## constants
+
+STARTING_POP = 40e6
+PLAGUE_KILLOFF = 0.6
+POST_COLLAPSE_KILLOFF = 2 / 3
+
+SURVIVING_POP = STARTING_POP * (1 - PLAGUE_KILLOFF) * (1 - POST_COLLAPSE_KILLOFF)
+years = 0:30
+
+## helper math functions
+
+#sigmoid function
+SIGMOID_K = 0.5
+function sigmoid(x::Number)::AbstractFloat
+    return 1 / (1 + exp(-SIGMOID_K * x))
+end
+
+## set annual growth rates year-by-year
+
+growth_rates = [(8 * (1 - exp(-0.5 * year)) - 4) + (4 * (sigmoid(-(year - 20)) - 1)) for year ∈ years]
+add_gauss!(growth_rates, 0.25)
+
+growth_plot = plot(years, growth_rates, legend=false)
+ylabel!(growth_plot, "Annual Growth Rate (%)")
+xlabel!(growth_plot, "Years Post-Collapse")
+title!(growth_plot, "Population Growth Rate of California Republic")
+
+## calculate population year-by-year
+
+populations = zeros(length(years))
+
+for i ∈ 1:length(populations)
+    if i == 1
+        populations[i] = SURVIVING_POP * (1 + (growth_rates[i] / 100))
+    else
+        populations[i] = populations[i - 1] * (1 + (growth_rates[i] / 100))
+    end
+end
+
+pop_plot = plot(years, populations, legend=false)
+ylabel!(pop_plot, "Population")
+xlabel!(pop_plot, "Years Post-Collapse")
+title!(pop_plot, "Population of the California Republic")
+
+## put both plots together
+
+plot(growth_plot, pop_plot, layout=(2,1), legend=false)
